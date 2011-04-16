9baf3b70a3314637ce49100d50e7e23ae9d65d5d
Tim Caswell <tim@creationix.com>
2011-04-16 02:20:42 -0700

    CamelCase where possible

--- window.js
+++ window.js
@@ -5,31 +5,31 @@ var Gtk = imports.gi.Gtk,
 
 Gtk.init(null, null);
 
-var current_filename;
-function load_file(filename) {
+var currentFilename;
+function loadFile(filename) {
 
-  current_filename = filename;
+  currentFilename = filename;
   window.title = "webOS IDE - " + filename;
 
   var file = Gio.file_new_for_path(filename);
-  source_buf.text = file.read().get_contents();
+  sourceBuf.text = file.read().get_contents();
 
 }
 
 
-var source_lang_mgr = new GtkSource.LanguageManager();
-var style_scheme_mgr = new GtkSource.StyleSchemeManager();
+var sourceLangMgr = new GtkSource.LanguageManager();
+var styleSchemeMgr = new GtkSource.StyleSchemeManager();
 
 var ui = new Gtk.Builder();
 ui.add_from_file("webOS_IDE.ui");
 var window = ui.get_object("window");
 
-var source_buf = new GtkSource.Buffer({
+var sourceBuf = new GtkSource.Buffer({
   highlight_matching_brackets: true,
-  language: source_lang_mgr.get_language("js"),
-  style_scheme: style_scheme_mgr.get_scheme("oblivion")
+  language: sourceLangMgr.get_language("js"),
+  style_scheme: styleSchemeMgr.get_scheme("oblivion")
 });
-var source_view = new GtkSource.View({
+var sourceView = new GtkSource.View({
   auto_indent: true,
 //  completion: ?,
   draw_spaces: 1 << 1 | 1 << 4,
@@ -44,13 +44,13 @@ var source_view = new GtkSource.View({
   smart_home_end: true,
   tab_width: 2
 });
-ui.get_object("scrolledwindow1").add(source_view);
+ui.get_object("scrolledwindow1").add(sourceView);
 
 
-source_view.set_buffer(source_buf);
-source_view.modify_font(Pango.font_description_from_string("monospace 11"));
+sourceView.set_buffer(sourceBuf);
+sourceView.modify_font(Pango.font_description_from_string("monospace 11"));
 
-load_file("window.js");
+loadFile("window.js");
 
 window.resize(1024, 600);
 window.show_all();
